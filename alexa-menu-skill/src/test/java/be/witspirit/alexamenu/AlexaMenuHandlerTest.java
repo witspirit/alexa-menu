6f304212436d0d183ccab02bf38b715df7930d0b
witspirit <witspirit@gmail.com>
2017-04-10 15:15:35 +0200

    Improved the AlexaMenuHandler test a bit to make it more readable and extract some boilerplate.

--- alexa-menu-skill/src/test/java/be/witspirit/alexamenu/AlexaMenuHandlerTest.java
+++ alexa-menu-skill/src/test/java/be/witspirit/alexamenu/AlexaMenuHandlerTest.java
@@ -1,8 +1,13 @@
 package be.witspirit.alexamenu;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 /**
  * Should more reflect an end-to-end test for the overall Alexa Menu Skill
@@ -11,35 +16,31 @@ public class AlexaMenuHandlerTest {
 
     @Test
     void helpIntent() throws IOException {
-        AlexaMenuHandler handler = new AlexaMenuHandler();
+        DocumentContext json = request("help");
 
-        String requestJson = "{\n" +
-                "  \"session\": {\n" +
-                "    \"sessionId\": \"SessionId.97e4fa40-bb54-4caf-9eef-e222c51e3504\",\n" +
-                "    \"application\": {\n" +
-                "      \"applicationId\": \"amzn1.ask.skill.51d5e161-2205-44b0-a0ea-aaf646d40a1e\"\n" +
-                "    },\n" +
-                "    \"attributes\": {},\n" +
-                "    \"user\": {\n" +
-                "      \"userId\": \"amzn1.ask.account.AF6NFJLGC6OF6K7PVCXQMYAC2ZSMHZDATQOYPEOMQTIEWHRPZJCBF4NUC7756SLUM2YTNOP3NJBCR7EZ4LQJN6QIZT3SE5BEVO2BUB7K2MXDUDI3CUISC3WC5NDWKSF3DDCVBWV2F4L2SFXUNX6QCDKACXQHGSRBGOJHEXEDCYOM73TUZGEP5PQADFW75U6NUQ6U53MANRWCYRI\"\n" +
-                "    },\n" +
-                "    \"new\": true\n" +
-                "  },\n" +
-                "  \"request\": {\n" +
-                "    \"type\": \"IntentRequest\",\n" +
-                "    \"requestId\": \"EdwRequestId.aaa96365-ac68-4d51-9a97-a764321bf280\",\n" +
-                "    \"locale\": \"en-US\",\n" +
-                "    \"timestamp\": \"2017-04-05T18:24:46Z\",\n" +
-                "    \"intent\": {\n" +
-                "      \"name\": \"AMAZON.HelpIntent\",\n" +
-                "      \"slots\": {}\n" +
-                "    }\n" +
-                "  },\n" +
-                "  \"version\": \"1.0\"\n" +
-                "}";
-        InputStream requestStream = new ByteArrayInputStream(requestJson.getBytes());
-        ByteArrayOutputStream responseStream = new ByteArrayOutputStream();
-        handler.handleRequest(requestStream, responseStream, null);
-        System.out.println(responseStream.toString());
+        assertThat(json.read("$.response.outputSpeech.text"), is("You can ask me 'what's for dinner'"));
+        assertThat(json.read("$.response.card.title"), is("Menu"));
+    }
+
+    @Test
+    void whatsForDinner() throws IOException {
+        DocumentContext json = request("whatsfordinner");
+
+        assertThat(json.read("$.response.outputSpeech.text"), is("We haven't decided yet"));
+    }
+
+    private DocumentContext request(String requestName) throws IOException {
+        try (FileInputStream requestStream = new FileInputStream("src/test/resources/requests/"+requestName+".json");
+             ByteArrayOutputStream responseStream = new ByteArrayOutputStream();) {
+
+            AlexaMenuHandler handler = new AlexaMenuHandler();
+            handler.handleRequest(requestStream, responseStream, null);
+
+            DocumentContext json = JsonPath.parse(responseStream.toString());
+
+            System.out.println(json.jsonString());
+
+            return json;
+        }
     }
 }
