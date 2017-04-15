c149b9b3368dcc31523f899d862ea4c6d17a85ef
witspirit <witspirit@gmail.com>
2017-04-15 13:35:17 +0200

    Prepared setup for deployment of the Lambda. Added another week menu

--- alexa-menu-skill/src/main/java/be/witspirit/alexamenu/FixedMenuRepository.java
+++ alexa-menu-skill/src/main/java/be/witspirit/alexamenu/FixedMenuRepository.java
@@ -15,5 +15,13 @@ public class FixedMenuRepository extends MapMenuRepository {
         set("20170412", "Pannenkoeken");
         set("20170413", "Diepvriespizza");
         set("20170414", "Spaghetti");
+
+        set("20170415", "Vogelnestjes");
+        set("20170416", "Macaroni");
+        set("20170417", "Pizza Hut / Koude rijst");
+        set("20170418", "Biefstuk Frietjes");
+        set("20170419", "Pitta");
+        set("20170420", "Kippebillen met rijst en curry");
+        set("20170421", "Spaghetti");
     }
 }
