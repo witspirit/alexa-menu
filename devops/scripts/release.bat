ee483599f9e2b2e4e3a2992326318d775adc848d
witspirit <witspirit@gmail.com>
2017-04-15 13:51:19 +0200

    Attempt nr.3

--- devops/scripts/release.bat
+++ devops/scripts/release.bat
@@ -1,13 +1,17 @@
 @echo off
 set RELVERSION=%1
 set NEWDEVVERSION=%2
-echo "Preparing release version: %RELVERSION"
-call mvn versions:set -DnewVersion=%RELVERSION
-call git commit -m "Setting release version to %RELVERSION"
-call git tag %RELVERSION
-echo "Preparing next development version: %NEWDEVVERSION"
-call mvn versions:set -DnewVerion=%NEWDEVVERSION
-call git commit -m "Setting next development version to %NEWDEVVERSION"
+
+echo "Preparing release version: %RELVERSION%"
+call mvn versions:set -DnewVersion=%RELVERSION%
+call git commit -m "Setting release version to %RELVERSION%"
+call git tag %RELVERSION%
+
+echo "Preparing next development version: %NEWDEVVERSION%"
+call mvn versions:set -DnewVersion=%NEWDEVVERSION%
+call git commit -m "Setting next development version to %NEWDEVVERSION%"
 
 REM call git push
 REM call git push --tags
+
+
