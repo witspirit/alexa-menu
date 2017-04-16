e9d5c064f2e203cd7da6024e65d0b3b0a55a3e0d
witspirit <witspirit@gmail.com>
2017-04-16 10:27:50 +0200

    Still not working... Tweaking some more.

--- devops/scripts/update_lambda.sh
+++ devops/scripts/update_lambda.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+echo Updating Alexa Menu Skill Lambda Code to version $1
 aws lambda update-function-code --function-name=alexa-menu-skill --zip-file=fileb://alexa-menu-skill/target/alexa-menu-skill-$1-jar-with-dependencies.jar
+echo Updating Alexa Menu Skill Lambda Configuration to version $1
 aws lambda update-function-configuration --function-name=alexa-menu-skill --role arn:aws:iam::138759191763:role/service-role/AlexaMenuSkillRole --handler be.witspirit.alexamenu.AlexaMenuHandler --description "Travis release build $1" --runtime java8
+echo Alexa Menu Skill Lambda Updated to version $1
