eeb19cf763660326edcf3071686baa5110b1c058
witspirit <witspirit@gmail.com>
2017-04-16 15:22:39 +0200

    Trying a workaround for the broken jars on the linux build.

--- devops/scripts/update_lambda.bat
+++ devops/scripts/update_lambda.bat
@@ -1,1 +1,3 @@
-aws lambda update-function-code --function-name=alexa-menu-skill --zip-file=fileb://..\..\alexa-menu-skill\target\alexa-menu-skill-0.3.0-SNAPSHOT-jar-with-dependencies.jar
+@echo off
+set version=%1
+aws lambda update-function-code --function-name=alexa-menu-skill --zip-file=fileb://..\..\alexa-menu-skill\target\alexa-menu-skill-%version%-lambda.jar
