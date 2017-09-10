1e46a6c2b6bbc84ff5c0cd3d669a11c5e4494ae7
witspirit <witspirit@gmail.com>
2017-09-10 16:28:00 +0200

    First steps towards actual Amazon Login interaction from my angular ui.

--- menu-ui/src/app/amazon-login.service.ts
+++ menu-ui/src/app/amazon-login.service.ts
@@ -4,15 +4,41 @@ import { Injectable } from '@angular/core';
 export class AmazonLoginService {
   loggedIn = false;
 
-  constructor() { }
+  constructor() {
+  }
 
   login() {
     console.log('login()');
     this.loggedIn = true;
+    const options = {scope: 'profile'};
+    amazon.Login.authorize(options, this.handleAmazonAuthorize);
+  }
+
+  handleAmazonAuthorize(response) {
+    console.log('handleAmazonAuthorize');
+
+    if (response.error) {
+      this.reportError('authorize', response);
+      return;
+    }
+    console.log('Access Token: ' + response.access_token);
+
+    // userInfo.accessToken = response.access_token;
+    // userInfo.authenticated = true;
+
+    // validate();
+
+   //  this.loggedIn = true;
+  }
+
+  reportError(context, errorResponse) {
+    console.log(context + ' ' + errorResponse.error + ' - ' + errorResponse.error_description);
+    alert(context + ' ' + errorResponse.error + ' - ' + errorResponse.error_description);
   }
 
   logout() {
     console.log('logout()');
+    amazon.Login.logout();
     this.loggedIn = false;
   }
 
