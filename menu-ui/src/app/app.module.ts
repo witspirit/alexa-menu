8d5363b3f442d7a84e3cb210e0c010029b235d02
witspirit <witspirit@gmail.com>
2017-09-24 17:54:27 +0200

    Tried more than a dozen approaches, but AOT is not working :-( settling for delivery without AOT.

--- menu-ui/src/app/app.module.ts
+++ menu-ui/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+// import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MenuMainComponent } from './menu-main/menu-main.component';
