5ec90dd6907dec297d9c02c2b64794c5cba1cce4
witspirit <witspirit@gmail.com>
2018-04-01 18:40:45 +0200

    Some attempts at reducing the startup time of the lambda...Doesn't seem to add up to anything significant. Still stuck with a startup time of 4s :-( Afterwards it is lightning fast... On my local PC the Spring Boot startup (including embedded tomcat) is around 1s...

--- lambdas/menu-api-gwlambda/src/main/java/be/witspirit/menuapigwlambda/MenuApiConfig.java
+++ lambdas/menu-api-gwlambda/src/main/java/be/witspirit/menuapigwlambda/MenuApiConfig.java
@@ -3,12 +3,60 @@ package be.witspirit.menuapigwlambda;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @SpringBootApplication
 public class MenuApiConfig extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MenuApiConfig.class, args);
+    }
+
+
+    // Some optimizations as copied from https://github.com/awslabs/aws-serverless-java-container/blob/master/samples/springboot/pet-store/src/main/java/com/amazonaws/serverless/sample/springboot/Application.java
+    // Must admit I don't fully understand the implications of these optimizations from a runtime behavior perspective.
+
+    /*
+     * Create required HandlerMapping, to avoid several default HandlerMapping instances being created
+     */
+    @Bean
+    public HandlerMapping handlerMapping() {
+        return new RequestMappingHandlerMapping();
+    }
+
+    /*
+     * Create required HandlerAdapter, to avoid several default HandlerAdapter instances being created
+     */
+    @Bean
+    public HandlerAdapter handlerAdapter() {
+        return new RequestMappingHandlerAdapter();
+    }
+
+    /*
+     * optimization - avoids creating default exception resolvers; not required as the serverless container handles
+     * all exceptions
+     *
+     * By default, an ExceptionHandlerExceptionResolver is created which creates many dependent object, including
+     * an expensive ObjectMapper instance.
+     */
+    @Bean
+    public HandlerExceptionResolver handlerExceptionResolver() {
+        return new HandlerExceptionResolver() {
+
+            @Override
+            public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+                return null;
+            }
+        };
     }
 
 }
