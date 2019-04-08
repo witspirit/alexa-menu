f9bb8a61a35aa857a82204bbea9752644ec04e2a
witspirit <witspirit@gmail.com>
2019-04-08 19:07:15 +0200

    Started work on an alternative UI implementation

---
+++ server-ui/src/main/java/be/witspirit/menu/serverui/ServerUiApplication.java
@@ -0,0 +1,14 @@
+package be.witspirit.menu.serverui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ServerUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerUiApplication.class, args);
+    }
+
+}
---
+++ server-ui/src/test/java/be/witspirit/menu/serverui/ServerUiApplicationTests.java
@@ -0,0 +1,16 @@
+package be.witspirit.menu.serverui;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServerUiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
